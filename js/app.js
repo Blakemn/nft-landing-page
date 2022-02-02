b708c5e417d95d25ba17fe9f91a471a0ded793d2
Blake Nguyen <blakenguyen2@gmail.com>
2022-02-01 17:20:55 -0700

    Name Changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'Divine Ducks';
 let editions = [];
 let dots = 1;
 
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${Divine Ducks}!! 🦆 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `Checking to see if you own any ${Divine Ducks} 🦆${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You own ${editions.length} ${Divine Ducks}!! 🦆`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `You don't own any ${Divine Ducks} 🦆`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
